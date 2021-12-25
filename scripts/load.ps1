e1e47aa4c0f17431ade85ec98efa9daea7aff003
Thomas Fike <fike@me.com>
2021-12-24 17:29:58 -0700

    chor: Add preconditons and return values

--- scripts/load.ps1
+++ scripts/load.ps1
@@ -1,3 +1,6 @@
+# Precondition: This script assumes that the root for a project is the current location.
+# Precondition: The project also needs to currently be compiled
+
 # Get the drives and check that only one was found
 $possible_drives = Get-Volume | Where-Object -FilterScript {$_.FileSystemLabel -like "RPI-RP2"}
 if (($possible_drives | measure | % {$_.Count}) -eq 1) {
@@ -5,7 +8,7 @@ if (($possible_drives | measure | % {$_.Count}) -eq 1) {
   Write-Output "There is more then 1 Pico Looking Device connected"
   Write-Output "Connected Options:"
   $possible_drives
-  exit
+  exit 1
 }
 
 $drive_letter = $possible_drives.DriveLetter
@@ -15,3 +18,4 @@ $pico_path = $drive_letter + ":\"
 $local_path = Get-Location
 $local_path = $local_path.tostring() + "\*"
 Copy-Item -Filter *.uf2 -Path $local_path -Destination $pico_path
+exit 0
